f681a1f8a17f4f680bbfc52c8fa88345b2556a15
Tyler Crammond <tylerchc92@gmail.com>
2016-01-26 21:33:31 +0000

    WIP gulp build task / gh pages

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var rev = require('gulp-rev');
 var revReplace = require('gulp-rev-replace');
 var uglify = require('gulp-uglify');
 
-gulp.task('build', function() {
+gulp.task('build', ['clean'], function() {
   return gulp.src('public/**.**')
     .pipe($.copy('dist/'))
     .pipe(uglify())             // Minify any javascript sources
     .pipe(rev())                // Rename the concatenated files
     .pipe(revReplace())         // Substitute in new filenames
   ;
+});
+
+gulp.task('clean', function() {
+  return gulp.src('dist/**/*')
+  .pipe(clean());
 });
